f17645fca046c0f48af560ebe9717cfe55a5bd52
Mary Nyamor <mnyamor@gmail.com>
2016-03-13 01:26:21 +0200

    Defining constants

--- constants/index.php
+++ constants/index.php
@@ -5,3 +5,13 @@
  * Date: 3/13/16
  * Time: 1:13 AM
  */
+
+/**
+ *  --- CONSTANTS ---
+ * Constants are similar to variables however, the value of a constant cannot be changed
+ * Constants are case sensitive and are all written in Caps
+ * In the example below, the constant is TITLE and the value is defining constants.
+ */
+define("TITLE" , "Defining Constants");
+
+echo(TITLE);
--- variables/index.php
+++ variables/index.php
@@ -6,14 +6,24 @@
  * Time: 1:13 AM
  */
 
+/**
+ *  --- VARIABLES ---
+ * We have: Strings, Boolean, Floating points, integers ..
+ */
+
 $my_favcolor = 'red';
 $my_name = 'Mary';
 $my_age = 14;
 
 print("My name is $my_name! <br/> 1'm $my_age years old and like the color $my_favcolor");
 
-//boolean - specifies a value of true or false
+/**
+ * Example of Booleans
+ */
 $logged_in = true;
 
-//Floating points -fractional number with a decimal point
+/**
+ * Example of floating points -fractional number with a decimal point
+ */
+
 $car_speed = 100.34;
