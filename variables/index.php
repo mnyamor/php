eb5e9008356359213d593c29b72d186ebf476ab2
Mary Nyamor <mnyamor@gmail.com>
2016-03-13 01:06:22 +0200

    variables

---
+++ variables/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>VARIABLES</title>
+</head>
+<body>
+<?php
+//defining variables
+//string
+
+$color = 'red';
+
+echo $color;
+
+//boolean - specifies a value of true or false
+$logged_in = true;
+
+//Floating points -fractional number with a decimal point
+$car_speed = 100.34;
+?>
+</body>
+</html>
