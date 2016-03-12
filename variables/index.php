16024a4d1350753348014f76c46af4cd53be5c94
Mary Nyamor <mnyamor@gmail.com>
2016-03-13 01:40:54 +0200

    calculate age

--- variables/index.php
+++ variables/index.php
@@ -13,7 +13,22 @@
 
 $my_favcolor = 'red';
 $my_name = 'Mary';
-$my_age = 14;
+$my_birthyear = 1990;
+
+/**
+ *  When using the date method, PHP has to know the timezone where the server resides
+ * in order to ouput the correct hour and date for the geo location
+ * The date_default_timezone_set takes a string that locates the server
+ * List of supported timezones can be found at php.net manual / timezones.php
+ */
+
+date_default_timezone_set('Finland/Helsinki');
+
+$date_today = date('F ,j,Y');
+$this_year = date('Y');
+
+//calculate current age
+$my_age = ($this_year -  $my_birthyear);
 
 print("My name is $my_name! <br/> 1'm $my_age years old and like the color $my_favcolor");
 
